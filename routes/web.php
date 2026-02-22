97c60fa784650fb2e992ea21b557dc0e5547a6ec
Salah <salah.57970@gmail.com>
2026-02-22 15:28:14 +0100

    fix web.php github issue

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-<<<<<<< HEAD
-    return view('welcome');
-});
-=======
     return view('equipments');
 })->name('equipments');
->>>>>>> 3de27ee (Add job scheduler, dymanic txt parser and first controller)
